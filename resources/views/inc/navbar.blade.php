1c8fdd8e7a649dee7eed9065e231c7d4277ee1f1
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2017-11-28 00:52:26 +0100

    Navbar adapt to screen size(smartphone) fixed

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,7 +1,18 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
   <a class="navbar-brand" href="/">Open-Review</a>
 
-  <div class="collapse navbar-collapse">
+  <button class="navbar-toggler"
+  type="button"
+  data-toggle="collapse"
+  data-target="#navbarsDefault"
+  aria-controls="navbarsDefault"
+  aria-expanded="false"
+  aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse"
+  id="navbarsDefault">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
