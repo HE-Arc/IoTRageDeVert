efada6d636b84ebf838876be1a44472a18a9b40e
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2017-11-27 17:56:42 +0100

    layouts organised, include folder in layouts, nav bar template ready to implement.

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,39 +1,41 @@
 <!-- Stored in resources/views/child.blade.php -->
-@extends('app')
+@extends('layouts.app')
+
 @section('title', 'Page Title')
+
 @section('sidebar')
     @parent
     <p>This is appended to the master sidebar.</p>
 @endsection
 
 @section('content')
-@php
-Use App\Article;
-$articles = App\Article::all();
-@endphp
-<div class="container-fluid">
-<h2>{{__('articles.header')}}</h2>
-@foreach($articles as $a)
-  <div class="article-container rounded">
-    <div class="row">
-      <div class="col-xs-12">
-        <div class="col-md-9 col-xs-12 bg-dark text-light rounded">
-          <h3><a class="text-light" href = "articles/{{$a->id}}">{{$a->title}}</a></h3>
-          <div class="author-line">
-            <p> {{ __('articles.by') }} <a href="#">{{$a->user()->first()->getName()}}</a></p>
+  @php
+    Use App\Article;
+    $articles = App\Article::all();
+  @endphp
+  <div class="container-fluid">
+    <h2>{{__('articles.header')}}</h2>
+    @foreach($articles as $a)
+      <div class="article-container rounded">
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="col-md-9 col-xs-12 bg-dark text-light rounded">
+              <h3><a class="text-light" href = "articles/{{$a->id}}">{{$a->title}}</a></h3>
+              <div class="author-line">
+                <p> {{ __('articles.by') }} <a href="#">{{$a->user()->first()->getName()}}</a></p>
+              </div>
+            </div>
+            <div class="col-md-3 col-xs-12 bg-light">
+              <a href="#"> Reviews (0)</a></br> <a href="#"> Submit a review </a>
+            </div>
           </div>
         </div>
-        <div class="col-md-3 col-xs-12 bg-light">
-          <a href="#"> Reviews (0)</a></br> <a href="#"> Submit a review </a>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-        <div class="col-md">
+        <div class="row">
+            <div class="col-md">
 
+            </div>
         </div>
-    </div>
-   </div>
-@endforeach
-</div>
+       </div>
+     @endforeach
+  </div>
 @endsection
