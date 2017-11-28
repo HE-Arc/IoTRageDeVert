3c5f8d8cd15428322c9da48e339296f7249679e9
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2017-11-28 02:32:03 +0100

    navbar login/logout display if auth -> Done!

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -20,21 +20,29 @@
       <li class="nav-item">
         <a class="nav-link" href="{{URL::to('/articles')}}">Articles</a>
       </li>
-
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</a>
-        <div class="dropdown-menu" aria-labelledby="dropdown01">
-          <a class="dropdown-item" href="{{URL::to('/login')}}">Log in</a>
-          <a class="dropdown-item" href="{{URL::to('/register')}}">Sign in</a>
-          <a class="dropdown-item" href="{{URL::to('/logout')}}">Log out</a>
-          <a class="dropdown-item" href="#">Messages</a>
-          <a class="dropdown-item" href="#">Settings</a>
-        </div>
-      </li>
+      @if (Auth::check())
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</a>
+          <div class="dropdown-menu" aria-labelledby="dropdown01">
+            <a class="dropdown-item" href="{{URL::to('/logout')}}">Log out</a>
+            <a class="dropdown-item" href="#">Messages</a>
+            <a class="dropdown-item" href="#">Settings</a>
+          </div>
+        </li>
+      @else
+        <li class="nav-item">
+          <a class="nav-link" href="{{URL::to('/login')}}">Log in</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{URL::to('/register')}}">Sign in</a>
+        </li>
+      @endif
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    <ul class="navbar-nav mr-auto ml-2">
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </ul>
   </div>
-  </nav>
+</nav>
