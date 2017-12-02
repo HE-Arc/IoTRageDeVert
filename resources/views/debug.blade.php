f63de41e5132cee9988079f5ebe033d68b3908f4
Guillaume Noguera <guillaume.noguera@gmail.com>
2017-12-02 18:20:44 +0100

    i'm pretty sad there

--- resources/views/debug.blade.php
+++ resources/views/debug.blade.php
@@ -1,9 +1,22 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="{{ asset('js/highlight.js') }}"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
 
-<div class="target-text">
-<p>The quick brown fox jumps over the lazy dog</p>
+<div class="container">
+  <div class="row">
+    <div class="col-9">
+      <div class="target-text">
+      <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas elit nulla, mollis a neque eu, mattis dictum elit. Ut sollicitudin porta purus, eget placerat orci vulputate at. Vivamus pretium velit id tristique tincidunt. Vivamus volutpat purus sit amet lacus pellentesque vestibulum quis et sem. Cras quis egestas dolor. Aenean eget tempor mauris. Sed nibh est, bibendum non tempor sed, mollis id erat. Nunc id nisl lacus. Nam mollis et orci at sodales. Vestibulum porta ut nulla quis ornare. Vivamus dictum tincidunt quam ac faucibus. Pellentesque vitae quam odio. Suspendisse in ligula blandit, elementum nibh ac, elementum quam. Aenean facilisis scelerisque mauris sed tristique. Phasellus fermentum massa dignissim arcu tempor tempus.
+      </div>
+    </div>
+    <div class="col-3 annotation-section">
+    </div>
+  </div>
 </div>
 <br />
-<div class="selected-text"></div>
