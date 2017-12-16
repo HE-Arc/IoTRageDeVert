61f624fd567540e4f4d3e0f0a896220038426a51
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2017-12-16 21:36:34 +0100

    reviews link

--- resources/views/myReviews.blade.php
+++ resources/views/myReviews.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>Your Reviews</h1>
+  <h1>Your reviews</h1>
   @php
     Use App\Review;
     $review = App\Review::where('user_id', Auth::id())->get();
