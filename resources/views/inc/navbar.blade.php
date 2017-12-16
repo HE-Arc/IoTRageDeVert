07dd7639034d2ee3449e5fb054f7c44879aac5b8
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2017-12-16 21:36:19 +0100

    reviews link

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -29,6 +29,7 @@
           <div class="dropdown-menu" aria-labelledby="dropdown01">
             <a class="dropdown-item" href="{{URL::to('/logout')}}">Log out</a>
             <a class="dropdown-item" href="{{URL::to('/myArticles')}}">My articles</a>
+            <a class="dropdown-item" href="{{URL::to('/myReviews')}}">My reviews</a>
             <a class="dropdown-item" href="#">Settings</a>
           </div>
         </li>
